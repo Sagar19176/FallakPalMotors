f5087e10717fe410b422eaa2f901e68d6e47e52b
Sagar19176 <sagarkumar19176@gmail.com>
2024-07-24 19:50:04 +0530

    Add files via upload

---
+++ MOD.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(link.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
